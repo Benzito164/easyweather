33783fb85970489ddc0b58f9ee95ce5360caafb7
Beni <beni@Benis-MBP.home>
2020-04-15 21:34:00 +0100

    initial commit

---
+++ easyweather/easyweather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  easyweather
+//
+//  Created by Beni Ibeh on 15/04/2020.
+//  Copyright © 2020 Beni Ibeh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
